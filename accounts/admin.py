c47f75e7177be4d4604b054602e7a86b235774ba
RaymondMR <ramonmontoya@outlook.com>
2025-08-30 20:55:14 -0600

    adding user

---
+++ accounts/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['username', 'email', 'name', 'is_staff', 'is_active']
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('name',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('name', 'email', 'password1', 'password2', 'is_staff', 'is_active')}),
+    )
+
+# Register your models here.
